ae3214fe820154a63b164c1cad949811044ed332
miao <ceshigongzhu@gmail.com>
2020-04-22 04:12:30 +0800

    date:4-22 time 4 am

--- src/components/FirstPages.js
+++ src/components/FirstPages.js
@@ -5,27 +5,51 @@ const FirstPages= () =>{
 
 
 
-const [imgs,setImgs]= useState(' ')
+const [imgs,setImgs]= useState([])
 
    useEffect(()=> {
         const getimgs = async () => {
             const response = await fetch(
-                `http://api.thecatapi.com/v1/images/search`
+                `https://api.thecatapi.com/v1/images/search?limit=19&page=10&order=Desc`
             );
             const data = await response.json();
+            const nums = data.length;
+            console.log(data.length);
             console.log(data[0]['url']);
-            setImgs(data[0]['url']);
+            let i;
+            let imgsArray = [];
+            for (i = 0 ; i<nums;i++){
+                imgsArray.push(data[i]['url']);
+            };
+            setImgs(imgsArray);
+            console.log(window.innerWidth);
         };
         getimgs();
     },[]
 );
 
+
+
+    let imgsClass = 'img-first';
+    let imgsList = 'imglist-first';
+    if (window.innerWidth>500){
+        imgsClass='img-second';
+        imgsList='imglist-second'
+    };
+
+const listImgs = imgs.map(
+    (imgsSingle)=>
+        <img className={imgsClass} alt="Loading--Cat" loading="lazy" src={imgsSingle}/>
+);
+
+
+
     return(
-        <div>
-            <p>432</p>
-            <img alt="Loading--Cat" src={imgs}/>
-
-
+        <div >
+            <h1>Cat Photos</h1>
+            <div className={imgsList}>
+            {listImgs}
+            </div>
         </div>
     )
 };
